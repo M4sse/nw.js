d02f2fafe93bd0f3e1edbf99b41a4c7d50c2c111
Marcel Ströcker <st.marcel23@googlemail.com>
2015-09-22 00:32:29 +0200

    [WIN] Drag Notification Window changes

--- src/api/clipboard/clipboard_mac.h
+++ src/api/clipboard/clipboard_mac.h
@@ -11,8 +11,9 @@ bool isPressed(unsigned short inKeyCode);
 void DoDragAndDropCocoa(std::vector<std::string> files,
 	content::Shell* shell);
 
-void registerNotificationWindow(std::string message_,
-                                content::Shell* shell);
+void registerNotificationWindow(std::string message_, 
+	content::Shell* shell);
+
 void createNotificationWindow();
 void closeNotificationWindow();
 
